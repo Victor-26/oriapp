6480804301022ef99bd47bc70409e4909ca85508
Victor-26 <katsar26.gm@gmail.com>
2021-06-16 17:29:35 +0900

    投稿機能の実装

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,11 +1,27 @@
 class CasesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
   end
 
   def new
+    @case = Case.new
   end
   
   def create
+    @case = Case.new(case_params)
+   if @case.save
+      redirect_to root_path
+    else
+      render :new
+   end
   end
-    
+
+  private
+  def case_params
+    params.require(:case).permit(:title,:explanation,:category_id,:image).merge(user_id: current_user.id)
+  
+  end 
+
 end
